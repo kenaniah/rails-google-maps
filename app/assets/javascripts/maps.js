06717a7572c415875c0df66f1ef9fcdbcae85996
Kenaniah Cerny <kenaniah@gmail.com>
2015-06-04 17:14:08 -0700

    Cleaned up some of the code, removing $.each and semicolons

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -1,43 +1,46 @@
 // The init function needs to run on load
-google.maps.event.addDomListener(window, 'load', initialize_my_map);
+google.maps.event.addDomListener(window, 'load', initialize_my_map)
 google.maps.event.addDomListener(window, 'page:load', initialize_my_map)
 
 // Define a function that should be ran on load (yay function hoisting)
 function initialize_my_map() {
 
     // Find the map DIV (if it exists)
-    var el = document.getElementById('address-map');
+    var el = document.getElementById('address-map')
 
     // Bail out if there's not an address map on the page
-    if(!el) return;
+    if(!el) return
 
     // Get an instance of the geocoder
-    var geocoder = new google.maps.Geocoder();
+    var geocoder = new google.maps.Geocoder()
 
     // Get the page's marker data from the JSON API
-    var url = window.location.origin + window.location.pathname + ".json";
+    var url = window.location.origin + window.location.pathname + ".json"
 
-    // Ajax the data URL
+    // Ajax the data URL (this retrieves the contents of that JSON url above)
     $.get(url, function(results){
 
         console.log("Data returned from " + url, results)
 
         // Wrap the data in an array if it's not one already
-        if(!(results instanceof Array)) results = [results];
+        if(!(results instanceof Array)) results = [results]
 
         // Perform geocoding for all addresses using promises to coordinate the results
-        var geo_promises = [];
+        var geo_promises = []
 
         // Geocode each address to be displayed
         // Realistically, this should be done in the Model when data is saved
-        $.each(results, function(idx, value){
+        for(var i = 0; i < results.length; i++){
+
+            // This creates promises using the jQuery Deferred library
             var promise = $.Deferred(function(deferred){
-                geocoder.geocode({'address': value.address}, function(geo_results, status){
+                geocoder.geocode({'address': results[i].address}, function(geo_results, status){
                     deferred.resolve(geo_results)
-                });
-            });
-            geo_promises.push(promise);
-        });
+                })
+            })
+            geo_promises.push(promise)
+
+        }
 
         // Dispatch the promises
         Promise.all(geo_promises).then(function(promise_results){
@@ -46,18 +49,21 @@ function initialize_my_map() {
             var mapProps = {
                 mapTypeId: google.maps.MapTypeId.ROADMAP
             }
-            var map = new google.maps.Map(el, mapProps);
+            var map = new google.maps.Map(el, mapProps)
 
             // Bounds are cool because they center our map for us
-            var bounds = new google.maps.LatLngBounds();
+            var bounds = new google.maps.LatLngBounds()
 
             // Track an array of our markers
-            var markers = [];
+            var markers = []
 
-            $.each(promise_results, function(idx, value){
+            // Loop over the promise results
+            for(var i = 0; i < promise_results.length; i++){
+
+                var promise_result = promise_results[i][0]
 
                 // result now represents a single geocoded address
-                var coord = value[0].geometry.location
+                var coord = promise_result.geometry.location
 
                 // Create and place a marker
                 var marker = new google.maps.Marker({position: coord})
@@ -69,19 +75,19 @@ function initialize_my_map() {
 
                 // Create an info window
                 var infowindow = new google.maps.InfoWindow({
-                    content: "<h1>" + results[idx].name + "</h1>" + value[0].formatted_address
-                });
+                    content: "<h1>" + results[i].name + "</h1>" + promise_result.formatted_address
+                })
 
                 // Open it above the marker
-                infowindow.open(map, markers[idx]);
+                infowindow.open(map, markers[i])
 
-            })
+            }
 
             // Center and fit the map using the bounds
-            map.fitBounds(bounds);
+            map.fitBounds(bounds)
 
         })
 
-    });
+    })
 
 }
